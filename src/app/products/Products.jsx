1bb123c7ce2e97ea4f37aee1b68de8308fa2a4a6
gsajdok <grzegorzsajdok@outlook.com>
2022-04-28 11:11:54 +0200

    Added color variables, set up initial header composition

--- src/app/products/Products.jsx
+++ src/app/products/Products.jsx
@@ -6,8 +6,28 @@ import { AppRoute } from '../../routing/AppRoute.enum';
 export const Products = () => {
   return (
     <>
+        <header>
+            <div className="header__wrapper">
+                <div className="logo">join.tsh.io</div>
+                <div className="account">
+                    <Link to={AppRoute.login}>Log in</Link>
+                </div>
+                <div className="searchBox">
+                    <input type="text" id="searchBox"/>
+                </div>
+                <div className="filters">
+                    <div className="filters__checkbox__active">
+                        <input type="checkbox" id="checkbox__active" name="checkbox__active"/>
+                        <label for="checkbox__active">Active</label>
+                    </div>
+                    <div className="filters__checkbox__promo">
+                        <input type="checkbox" id="checkbox__promo" name="checkbox__promo"/>
+                        <label htmlFor="checkbox__promo">Promo</label>
+                    </div>
+                </div>
+            </div>
+        </header>
       <h2>Products page</h2>
-      <Link to={AppRoute.login}> Login </Link>
     </>
   );
 };
