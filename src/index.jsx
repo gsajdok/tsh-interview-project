fd809cc3afcc1a992539507072fbf206b11971c1
gsajdok <grzegorzsajdok@outlook.com>
2022-04-28 10:22:09 +0200

    Installed sass package, added scss structure, imported css reset

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import { AppProviders } from './providers/AppProviders';
 import { App } from './app/App';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/main.scss';
+
 ReactDOM.render(
   <AppProviders>
     <App />
