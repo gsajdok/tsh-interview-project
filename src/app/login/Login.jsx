03396234e57da7d1fb0e15cdef1c8b0bd17e3f32
gsajdok <grzegorzsajdok@outlook.com>
2022-05-03 21:39:44 +0200

    Added the login page with RWD

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,28 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+
 
 import { AppRoute } from '../../routing/AppRoute.enum';
 
+import loginPicture from '../../assets/images/loginPicture.png'
+
 export const Login = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
   return (
-    <>
-      <Link to={AppRoute.home}>Products page</Link>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label>
-            username:
-            <input name="username" />
-          </label>
-        </div>
-        <div>
-          <label>
-            password:
-            <input name="password" type="password" />
-          </label>
-        </div>
-        <button type="submit">submit</button>
-      </form>
-    </>
+      <div className="loginPage__wrapper">
+          <div className="loginPage__image">
+              <img src={loginPicture} alt="login picture"/>
+          </div>
+          <div className="loginPage__form">
+              <header>
+                  <div className="header__wrapper">
+                      <div className="logo"><h1 className="logo"><Link to={AppRoute.home}>join.tsh.io</Link></h1></div>
+                  </div>
+              </header>
+              <div className="login">
+                  <div className="login__wrapper">
+                      <div className="login__box">
+                          <div className="login__title">
+                              <h2>Login</h2>
+                          </div>
+                          <div className="login__form">
+                              <form onSubmit={handleSubmit}>
+                                  <label>
+                                      <h3>Username</h3>
+                                      <input
+                                          type="text"
+                                          value={name}
+                                          placeholder="Enter username"
+                                          name="username"
+                                          onChange={e => setName(e.target.value)}
+                                      />
+                                  </label>
+                                  <label>
+                                      <h3>Password</h3>
+                                      <input
+                                          type="password"
+                                          value={password}
+                                          placeholder="Enter password"
+                                          name="password"
+                                          onChange={e => setPassword(e.target.value)}
+                                      />
+                                  </label>
+                                  <div className="login__button">
+                                      <input type="submit" value="Submit" className="button button--filled"/>
+                                  </div>
+                              </form>
+                          </div>
+                          <a className="login__link link--recovery">Forgot password?</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
   );
 };
