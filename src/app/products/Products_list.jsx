75a6c4f176bf27cd9db7a8eb337470a6bdc56002
gsajdok <grzegorzsajdok@outlook.com>
2022-05-05 03:59:05 +0200

    Stopped rendering the pagination wrapper if no need for pagination

--- src/app/products/Products_list.jsx
+++ src/app/products/Products_list.jsx
@@ -9,9 +9,10 @@ export const Products_list = ({products, setCurrentPage}) => {
             <div className="products__list">
                 {products.meta.totalItems===0 ? <Empty/> : products.items.map(item => <Product key={item.id} item={item}/>)}
             </div>
+            {products.meta.totalItems>0 &&
             <div className="products__pages">
                 <Pagination metaData={products.meta} setCurrentPage={setCurrentPage}/>
-            </div>
+            </div>}
         </>
     )
 }
