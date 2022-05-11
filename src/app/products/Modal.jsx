29c98421db1b2a1e44ea5a81001e5e91d8e567ac
gsajdok <grzegorzsajdok@outlook.com>
2022-05-11 15:35:25 +0200

    can now close the modal window by clicking outside of it

--- src/app/products/Modal.jsx
+++ src/app/products/Modal.jsx
@@ -1,15 +1,32 @@
 import {ReactComponent as Cross} from '../../assets/icons/Cross.svg'
+import {useEffect, useRef} from "react";
+
+function onClickOutside(ref, callback) {
+    useEffect(() => {
+        function handleClickOutside(e) {
+            if(ref.current && !ref.current.contains(e.target)) {
+                callback();
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [ref])
+}
 
 export const Modal = ({item, setShowModal}) => {
+    const ref = useRef(null);
     const handleOnClick = () => {
         setShowModal(false);
         document.body.style.overflow = "auto";
     }
+    onClickOutside(ref, handleOnClick);
 
     return (
         <div className="modal">
             <div className="modal__wrapper">
-                <div className="modal__window">
+                <div className="modal__window" ref={ref}>
                     <div className="modal__close">
                         <button data-testid="modalCloseButton" onClick={handleOnClick} className="modal__close__button"><Cross/></button>
                     </div>
